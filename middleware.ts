a189f83b8f9f8ec918b76d5c8c1a71b4e86be457
balldev1 <balldev1@hotmail.com>
2025-03-16 16:09:03 +0700

    test

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,19 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(req: Request) {
-    // ดึงค่า cookies จาก headers
-    const cookieHeader = req.headers.get('cookie');
-    const cookies = new Map(cookieHeader?.split(';').map(cookie => cookie.trim().split('=')) || []);
-    // ตรวจสอบว่า _vercel_jwt มีอยู่ใน cookies หรือไม่
-    const vercelJwt = cookies.get('_vercel_jwt');
-    console.log(vercelJwt)
-    // ถ้าไม่มี _vercel_jwt และไม่ได้อยู่ในหน้า /login ให้ redirect ไปที่หน้า login
-    if (!vercelJwt && req.url.includes('/login') === false) {
+export async function middleware(req: any) {
+    // ดึง JWT จาก cookies
+    const jwt = req.cookies.get('next-auth.session-token');
+
+    // หากไม่มี JWT ให้ redirect ไปที่หน้า login
+    if (!jwt) {
         return NextResponse.redirect(new URL('/login', req.url));
     }
 
-    // ถ้ามี _vercel_jwt หรืออยู่ในหน้า /login ให้ดำเนินการต่อไป
+    // ดำเนินการปกติ ถ้ามี JWT
     return NextResponse.next();
 }
 
-// กำหนดให้ middleware ทำงานกับทุกเส้นทาง ยกเว้น /login
+// กำหนด path ที่ต้องการให้ middleware ทำงาน
 export const config = {
-    matcher: ['/login'],
+    matcher: ['/cart', '/'], // middleware จะทำงานเฉพาะกับเส้นทางเหล่านี้
 };
